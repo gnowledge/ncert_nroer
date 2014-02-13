f9b1c3f5061bb5cef5d6ee197f14efd35ef27764
Anuja G <anuja.gk@gmail.com>
2014-02-13 15:31:52 +0530

    edited get_Concepts

--- gstudio/get_concepts_with_relns.py
+++ gstudio/get_concepts_with_relns.py
@@ -5,18 +5,19 @@ def con_script():
     all_sys=System.objects.all()                                                                                                          
     all_wikis=[]                                                                                                                        
     fp=open("../demo/concept_with_relns.csv","a")                                                                                        
-    for each in all_sys:                                                                                                                 
+    for each in all_sys[0:5]:                                                                                                               
         if wiki_st in each.systemtypes.all():                                                                                            
             all_wikis.append(each)
-    for eachwiki in all_wikis[0:5]:
+    fp.write("subject-id,subject-name,relation-name,object-name,object-title,subject-title")   
+    for eachwiki in all_wikis:
         line="\n"+eachwiki.id.__str__()+","+eachwiki.title.encode('utf8')+","                                                            
-        fp.write(line)                                                                                                                   
         relns=each.get_relations_for_view()                                                                                              
         for key,value in relns.items():                                                                                                  
             for eachval in value:                                                                                                        
                 obj=Node.objects.get(id=eachval['id'])                                                                                   
-                lpline=key+","+obj.id+","+obj.title.encode('utf8')                                                                       
+                lpline=key+","+obj.id+","+obj.title.encode('utf8')+","+eachwiki.title.encode('utf8')                                        
                 fline=line+lpline                                                                                                        
+                print fline
                 fp.write(fline)                                                                                                          
     fp.close()                                                                                                                            
     return
