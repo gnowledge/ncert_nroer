bc45b6af73dfcc9309978da6cc64dd304d97d2f0
Anuja G <anuja.gk@gmail.com>
2014-02-14 11:01:32 +0530

    modified script to omit resources

--- gstudio/get_concept_relns_omitted.py
+++ gstudio/get_concept_relns_omitted.py
@@ -13,12 +13,13 @@ def con_script():
     for eachwiki in all_wikis:
         line="\n"+eachwiki.id.__str__()+","+eachwiki.title.encode('utf8')+","                                                            
         relns=eachwiki.get_relations_for_view()
-        for key,value in relns.items():                                                                                                  
-            for eachval in value:   
-                oburl=str(eachval['url']).split("/")                                                                                        
-                objid=oburl[len(oburl)-1]
-                lpline=str(key)+","+eachval['title'].encode('utf8')+","+str(objid)+","+eachwiki.title.encode('utf8')
-                fline=line+lpline                                                                                                        
-                fp.write(fline)                                                                                                          
+        for key,value in relns.items():
+            if not (key=="has_video" or key =="has_image"):
+                for eachval in value:   
+                    oburl=str(eachval['url']).split("/")
+                    objid=oburl[len(oburl)-1]
+                    lpline=str(key)+","+str(objid)+","+eachval['title'].encode('utf8')+","+eachwiki.title.encode('utf8')
+                    fline=line+lpline
+                    fp.write(fline)                                                                                                    
     fp.close()                                                                                                                            
     return
